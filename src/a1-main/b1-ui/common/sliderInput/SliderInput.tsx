d3d935ebd9b83daad7bce88dd5e849d0531d4f48
R3laxation <memories@bk.ru>
2022-06-08 12:19:07 +0600

    Added SearchField.tsx and SliderInput.tsx

--- src/a1-main/b1-ui/common/sliderInput/SliderInput.tsx
+++ src/a1-main/b1-ui/common/sliderInput/SliderInput.tsx
@@ -3,34 +3,35 @@ import s from "./SliderInput.module.css"
 import {SuperDoubleRange} from "./superDoubleRange/SuperDoubleRange";
 import {useAppDispatch, useAppSelector} from "../../../b2-bll/store";
 import {setMaxCards, setMinCards} from "../../../b2-bll/packsReducer";
-import {useDebounce} from "../utilsFunc/useDebounceHOOK/useDebounce";
+import {useDebounce} from "../utilsFunc/useDebounce/useDebounce";
+
 
 export function SliderInput() {
 
-    const min = useAppSelector((state) => state.packs.min)
-    const max = useAppSelector((state) => state.packs.max)
-    const maxCardsCount = useAppSelector((state) => state.packs.maxCardsCount)
+    const maxCards = useAppSelector((state) => state.packs.maxCardsCount)
+
     const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        dispatch(setMaxCards(maxCardsCount))
-    }, [maxCardsCount])
-
     const [value1, setValue1] = useState<number>(0)
-    const [value2, setValue2] = useState<number>(0)
-    //
+    const [value2, setValue2] = useState<number>(maxCards)
+
     const delayedMinValue = useDebounce(value1, 1500);
     const delayedMaxValue = useDebounce(value2, 1500);
 
     // const minGap = 5;
-
     const onChangeRangeHandler = (value: [number, number]) => {
         setValue1(value[0])
         setValue2(value[1])
-        delayedMinValue !== min && dispatch(setMinCards(delayedMinValue))
-        delayedMaxValue !== max && dispatch(setMaxCards(delayedMaxValue))
     }
 
+    useEffect(() => {
+        dispatch(setMinCards(delayedMinValue))
+        dispatch(setMaxCards(delayedMaxValue))
+    }, [delayedMinValue, delayedMaxValue])
+
+    useEffect(() => {
+        setValue2(maxCards)
+    }, [maxCards])
 
 
     return (
@@ -45,7 +46,8 @@ export function SliderInput() {
                 <SuperDoubleRange
                     commonValue={[value1, value2]}
                     onChangeRange={onChangeRangeHandler}
-                    max={maxCardsCount}
+
+
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
