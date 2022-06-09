b2fe822d102435e209b614ccf43a202a98168a36
SRGREEN90 <you@example.com>
2022-06-09 14:20:45 +0300

    merge fix3

--- src/a1-main/b1-ui/common/packFrame/myProfile/myProfile.tsx
+++ src/a1-main/b1-ui/common/packFrame/myProfile/myProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {PackFrame} from "../packFrame/PackFrame";
 import noAvatar from './noAvatar.png'
 import {useAppSelector} from "../../../../b2-bll/store";
@@ -9,12 +9,16 @@ import Packs from '../../../../../a2-features/b3-cards/c1-packs/Packs';
 import {DoubleCheckbox} from "../doubleCheckbox/DoubleCheckBox";
 import {SearchField} from "../../searchField/SearchField";
 import {NavLink} from "react-router-dom";
+import Pagination from "../../pagination/Pagination";
+import SuperSelect from "../../seperSelect/SuperSelect";
+import SuperButton from "../../superButton/SuperButton";
 
 
 const MyProfile = () => {
     const avatar = useAppSelector<string | undefined>(state => state.profile.avatar)
     const name = useAppSelector<string>(state => state.profile.name)
-
+    const arrValue = ['5','10','15','20']
+    const [value, setValue] = useState(arrValue[0])
 
     return <div>
 
@@ -33,18 +37,20 @@ const MyProfile = () => {
                     <NavLink to={'/edit-c1-profile'}>
                         <button className={s.button}>Edit profile</button>
                     </NavLink>
-
-
-                    <SliderInput/>
-
+                    <div>
+                        <SliderInput/>
+                    </div>
 
                 </div>
             </div>
 
             <div className={s.packs}>
                 <Packs/>
+                <div className={s.pagination}>
+                    <Pagination/>
+                   <SuperSelect value={value} options={arrValue} onChangeOption={setValue}/>
+                </div>
             </div>
-
 
         </PackFrame>
 
