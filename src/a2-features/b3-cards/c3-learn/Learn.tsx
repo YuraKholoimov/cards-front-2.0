baa6b539f67667ecb502f113ac9e2339af894296
Yura Kholoimov <yurakholoimov@ya.ru>
2022-06-13 11:33:22 +0300

    Added SuperRateComponent for Learn page

--- src/a2-features/b3-cards/c3-learn/Learn.tsx
+++ src/a2-features/b3-cards/c3-learn/Learn.tsx
@@ -2,27 +2,29 @@ import React, {ReactNode, useEffect, useState} from 'react';
 import SuperButton from "../../../a1-main/b1-ui/common/superButton/SuperButton";
 
 import s from './learn.module.css'
-import {Navigate, useNavigate, useParams} from "react-router-dom";
-import {cardsApi} from "../../../a1-main/b3-dal/cardsApi";
-import {useDispatch, useSelector} from "react-redux";
+import {useNavigate, useParams} from "react-router-dom";
+import {useDispatch} from "react-redux";
 import {useAppSelector} from "../../../a1-main/b2-bll/store";
 import {PATH} from "../../../a1-main/b1-ui/routes/RoutesComponent";
-import {CardsType, learnCardsThunk, setCards, setCardsThunk} from '../../../a1-main/b2-bll/cardsReducer';
-import Preloader from "../../../a1-main/b1-ui/common/preloader/Preloader";
+import {learnCardsThunk, setCardsThunk} from '../../../a1-main/b2-bll/cardsReducer';
+import SuperRadioSelect from "../../../a1-main/b1-ui/common/SuperRadioSelect/SuperRadioSelect";
 
 type PropsType = {
     children?: ReactNode
 }
+
 export const Learn: React.FC<PropsType> = ({children}) => {
+
+    const grades = ["Did not know", "Forgot", "A lot of thought", "Confused", "Knew the answer"];
+
     const dispatch = useDispatch<any>()
     const navigate = useNavigate()
     const {packId} = useParams<{packId: string}>()
-    console.log(packId)
     const packName = useAppSelector<string>(state => state.packs.cardsPack.filter((p: any) => p._id === packId)[0]?.name)
     const [isVisible, setIsVisible] = useState(false)
+    const [rating, setRating] = useState("")
 
     const cards = useAppSelector<any>(state => state.cards.cards)
-    console.log(cards)
     const isLoading = useAppSelector<boolean>(state => state.app.isLoading)
 
     useEffect(() => {
@@ -37,6 +39,21 @@ export const Learn: React.FC<PropsType> = ({children}) => {
                 <p><b>Question: </b>
                     {cards[0].question}
                 </p>
+
+            {
+                isVisible && <>
+                    <h3>Answer:</h3>
+                <p>{cards[0].answer}</p>
+                <h3>Rate yourself:</h3>
+                    <SuperRadioSelect
+                        name={'radio'}
+                        options={grades}
+                        value={rating}
+                        onChangeOption={setRating}
+                    />
+                </>
+
+            }
 
             <nav>
                 <SuperButton onClick={() => navigate(PATH.PACKS)}>Cansel</SuperButton>
